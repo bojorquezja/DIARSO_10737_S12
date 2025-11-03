24f2609cc4a0feef98396ee10ca229191bd32c18
bojorquezja <bojorquez.ja@gmail.com>
2025-11-03 18:12:22 -0500

    proxy

---
+++ proxy/Proxy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proxy {
+    internal class Proxy {
+        public static double TipoCambio() {
+            double cambio = Sunat.TipoCambio();
+            Console.WriteLine($"guardado {cambio} en BD");
+            return cambio;
+        }
+    }
+}
